SysAdminDoc/uBlockVanced

commit 9df9c27428617800539f1894b31f95d747b48684

Allow use of browser.storage.local as cache storage backend in Firefox
Related issue:
- https://github.com/uBlockOrigin/uBlock-issues/issues/409

By default `indexedDB` is used in Firefox for purpose of cache storage
backend.

This commit allows to force the use of `browser.storage.local` instead
as cache storage backend. For this to happen, set `cacheStorageAPI` to
`browser.storage.local` in advanced settings.

Additionally, should `indexedDB` not be available for whatever reason,
uBO will automatically fallback to `browser.storage.local`.

src/js/storage.js
@@ -50,7 +50,7 @@
         countdown += 1;
         vAPI.storage.getBytesInUse(null, process);
     }
-    if ( this.cacheStorage !== vAPI.storage ) {
+    if ( this.cacheStorage.name !== 'browser.storage.local' ) {
         countdown += 1;
         this.assets.getBytesInUse().then(count => {
             process(count);
@@ -91,8 +91,13 @@
 /******************************************************************************/
 
 µBlock.loadHiddenSettings = function() {
+    return new Promise(resolve => {
+    // >>>> start of executor
+
     vAPI.storage.get('hiddenSettings', bin => {
-        if ( bin instanceof Object === false ) { return; }
+        if ( bin instanceof Object === false ) {
+            return resolve();
+        }
         const hs = bin.hiddenSettings;
         if ( hs instanceof Object ) {
             const hsDefault = this.hiddenSettingsDefault;
@@ -110,6 +115,10 @@
             this.saveImmediateHiddenSettings();
         }
         self.log.verbosity = this.hiddenSettings.consoleLogLevel;
+        resolve();
+    });
+
+    // <<<< end of executor
     });
 };
 
@@ -195,9 +204,6 @@
     );
 };
 
-// Do this here to have these hidden settings loaded ASAP.
-µBlock.loadHiddenSettings();
-
 /******************************************************************************/
 
 µBlock.savePermanentFirewallRules = function() {
@@ -240,24 +246,29 @@
 
 **/
 
-µBlock.loadSelectedFilterLists = function(callback) {
-    var µb = this;
-    vAPI.storage.get('selectedFilterLists', function(bin) {
+µBlock.loadSelectedFilterLists = function() {
+    return new Promise(resolve => {
+    // >>>> start of executor
+
+    vAPI.storage.get('selectedFilterLists', bin => {
         // Select default filter lists if first-time launch.
-        if ( !bin || Array.isArray(bin.selectedFilterLists) === false ) {
-            µb.assets.metadata(function(availableLists) {
-                µb.saveSelectedFilterLists(
-                    µb.autoSelectRegionalFilterLists(availableLists)
+        if (
+            bin instanceof Object === false ||
+            Array.isArray(bin.selectedFilterLists) === false
+        ) {
+            this.assets.metadata(function(availableLists) {
+                this.saveSelectedFilterLists(
+                    this.autoSelectRegionalFilterLists(availableLists)
                 );
-                callback();
+                resolve();
             });
             return;
         }
-        // TODO: Removes once 1.1.15 is in widespread use.
-        // https://github.com/gorhill/uBlock/issues/3383
-        vAPI.storage.remove('remoteBlacklists');
-        µb.selectedFilterLists = bin.selectedFilterLists;
-        callback();
+        this.selectedFilterLists = bin.selectedFilterLists;
+        resolve();
+    });
+
+    // <<<< end of executor
     });
 };
 
@@ -1130,16 +1141,16 @@
 // necessarily present, i.e. administrators may removed entries which
 // values are left to the user's choice.
 
-µBlock.restoreAdminSettings = function(callback) {
-    // Support for vAPI.adminStorage is optional (webext).
+µBlock.restoreAdminSettings = function() {
+    return new Promise(resolve => {
+    // >>>> start of executor
+
     if ( vAPI.adminStorage instanceof Object === false ) {
-        callback();
-        return;
+        return resolve();
     }
 
-    var onRead = function(json) {
-        var µb = µBlock;
-        var data;
+    vAPI.adminStorage.getItem('adminSettings', json => {
+        let data;
         if ( typeof json === 'string' && json !== '' ) {
             try {
                 data = JSON.parse(json);
@@ -1148,13 +1159,12 @@
             }
         }
 
-        if ( typeof data !== 'object' || data === null ) {
-            callback();
-            return;
+        if ( data instanceof Object === false ) {
+            return resolve();
         }
 
-        var bin = {};
-        var binNotEmpty = false;
+        const bin = {};
+        let binNotEmpty = false;
 
         // Allows an admin to set their own 'assets.json' file, with their own
         // set of stock assets.
@@ -1164,8 +1174,8 @@
         }
 
         if ( typeof data.userSettings === 'object' ) {
-            for ( var name in µb.userSettings ) {
-                if ( µb.userSettings.hasOwnProperty(name) === false ) {
+            for ( const name in this.userSettings ) {
+                if ( this.userSettings.hasOwnProperty(name) === false ) {
                     continue;
                 }
                 if ( data.userSettings.hasOwnProperty(name) === false ) {
@@ -1208,13 +1218,14 @@
         }
 
         if ( typeof data.userFilters === 'string' ) {
-            µb.assets.put(µb.userFiltersPath, data.userFilters);
+            this.assets.put(this.userFiltersPath, data.userFilters);
         }
 
-        callback();
-    };
+        resolve();
+    });
 
-    vAPI.adminStorage.getItem('adminSettings', onRead);
+    // <<<< end of executor
+    });
 };
 
 /******************************************************************************/

platform/chromium/vapi-background.js
@@ -91,7 +91,6 @@ vAPI.app.restart = function() {
 // chrome.storage.local.get(null, function(bin){ console.debug('%o', bin); });
 
 vAPI.storage = chrome.storage.local;
-vAPI.cacheStorage = chrome.storage.local;
 
 /******************************************************************************/
 /******************************************************************************/

src/js/assets.js
@@ -396,13 +396,17 @@ const updateAssetSourceRegistry = function(json, silent) {
 
 const getAssetSourceRegistry = function(callback) {
     if ( assetSourceRegistryPromise === undefined ) {
-        assetSourceRegistryPromise = new Promise(resolve => {
-        // start of executor
-        µBlock.cacheStorage.get('assetSourceRegistry', bin => {
+        assetSourceRegistryPromise = µBlock.cacheStorage.get(
+            'assetSourceRegistry'
+        ).then(bin => {
             if (
-                bin instanceof Object === false ||
-                bin.assetSourceRegistry instanceof Object === false
+                bin instanceof Object &&
+                bin.assetSourceRegistry instanceof Object
             ) {
+                assetSourceRegistry = bin.assetSourceRegistry;
+                return;
+            }
+            return new Promise(resolve => {
                 api.fetchText(
                     µBlock.assetsBootstrapLocation || 'assets/assets.json',
                     details => {
@@ -410,12 +414,7 @@ const getAssetSourceRegistry = function(callback) {
                         resolve();
                     }
                 );
-                return;
-            }
-            assetSourceRegistry = bin.assetSourceRegistry;
-            resolve();
-        });
-        // end of executor
+            });
         });
     }
 
@@ -451,16 +450,15 @@ let assetCacheRegistry = {};
 
 const getAssetCacheRegistry = function() {
     if ( assetCacheRegistryPromise === undefined ) {
-        assetCacheRegistryPromise = new Promise(resolve => {
-            µBlock.cacheStorage.get('assetCacheRegistry', bin => {
-                if (
-                    bin instanceof Object &&
-                    bin.assetCacheRegistry instanceof Object
-                ) {
-                    assetCacheRegistry = bin.assetCacheRegistry;
-                }
-                resolve();
-            });
+        assetCacheRegistryPromise = µBlock.cacheStorage.get(
+            'assetCacheRegistry'
+        ).then(bin => {
+            if (
+                bin instanceof Object &&
+                bin.assetCacheRegistry instanceof Object
+            ) {
+                assetCacheRegistry = bin.assetCacheRegistry;
+            }
         });
     }
 
@@ -509,8 +507,11 @@ const assetCacheRead = function(assetKey, callback) {
         reportBack(bin[internalKey]);
     };
 
-    getAssetCacheRegistry().then(( ) => {
-        µBlock.cacheStorage.get(internalKey, onAssetRead);
+    Promise.all([
+        getAssetCacheRegistry(),
+        µBlock.cacheStorage.get(internalKey),
+    ]).then(results => {
+        onAssetRead(results[1]);
     });
 };
 
@@ -537,17 +538,16 @@ const assetCacheWrite = function(assetKey, details, callback) {
             entry.remoteURL = details.url;
         }
         µBlock.cacheStorage.set(
-            { [internalKey]: content },
-            details => {
-                if (
-                    details instanceof Object &&
-                    typeof details.bytesInUse === 'number'
-                ) {
-                    entry.byteLength = details.bytesInUse;
-                }
-                saveAssetCacheRegistry(true);
+            { [internalKey]: content }
+        ).then(details => {
+            if (
+                details instanceof Object &&
+                typeof details.bytesInUse === 'number'
+            ) {
+                entry.byteLength = details.bytesInUse;
             }
-        );
+            saveAssetCacheRegistry(true);
+        });
         const result = { assetKey, content };
         if ( typeof callback === 'function' ) {
             callback(result);
@@ -556,9 +556,7 @@ const assetCacheWrite = function(assetKey, details, callback) {
         fireNotification('after-asset-updated', result);
     };
 
-    getAssetCacheRegistry().then(( ) => {
-        µBlock.cacheStorage.get(internalKey, onReady);
-    });
+    getAssetCacheRegistry().then(( ) => onReady());
 };
 
 const assetCacheRemove = function(pattern, callback) {

src/js/messaging.js
@@ -848,17 +848,17 @@ var restoreUserData = function(request) {
 // Remove all stored data but keep global counts, people can become
 // quite attached to numbers
 
-var resetUserData = function() {
+const resetUserData = function() {
     let count = 3;
-    let countdown = ( ) => {
+    const countdown = ( ) => {
         count -= 1;
         if ( count === 0 ) {
             vAPI.app.restart();
         }
     };
-    µb.cacheStorage.clear(countdown); // 1
-    vAPI.storage.clear(countdown);    // 2
-    µb.saveLocalSettings(countdown);  // 3
+    µb.cacheStorage.clear().then(( ) => countdown()); // 1
+    vAPI.storage.clear(countdown);             // 2
+    µb.saveLocalSettings(countdown);           // 3
     vAPI.localStorage.removeItem('immediateHiddenSettings');
 };
 

src/js/start.js
@@ -333,7 +333,7 @@ const fromFetch = function(to, fetched) {
 
 /******************************************************************************/
 
-const onSelectedFilterListsLoaded = function() {
+const onSelectedFilterListsReady = function() {
     log.info(`List selection ready ${Date.now()-vAPI.T0} ms after launch`);
 
     const fetchableProps = {
@@ -371,6 +371,16 @@ const onSelectedFilterListsLoaded = function() {
 
 /******************************************************************************/
 
+const onHiddenSettingsReady = function() {
+    return µb.cacheStorage.select(
+        µb.hiddenSettings.cacheStorageAPI
+    ).then(backend => {
+        log.info(`Backend storage for cache will be ${backend}`);
+    });
+};
+
+/******************************************************************************/
+
 // TODO(seamless migration):
 // Eventually selected filter list keys will be loaded as a fetchable
 // property. Until then we need to handle backward and forward
@@ -379,14 +389,24 @@ const onSelectedFilterListsLoaded = function() {
 
 const onAdminSettingsRestored = function() {
     log.info(`Admin settings ready ${Date.now()-vAPI.T0} ms after launch`);
-    µb.loadSelectedFilterLists(onSelectedFilterListsLoaded);
+
+    Promise.all([
+        µb.loadHiddenSettings().then(( ) =>
+            onHiddenSettingsReady()
+        ),
+        µb.loadSelectedFilterLists(),
+    ]).then(( ) =>
+        onSelectedFilterListsReady()
+    );
 };
 
 /******************************************************************************/
 
 return function() {
     // https://github.com/gorhill/uBlock/issues/531
-    µb.restoreAdminSettings(onAdminSettingsRestored);
+    µb.restoreAdminSettings().then(( ) => {
+        onAdminSettingsRestored();
+    });
 };
 
 /******************************************************************************/

commit 2e8e11e84f182e2e618a3a510a01834b2789caec

Fix spurious output at uBO's dev console
Regression from https://github.com/gorhill/uBlock/commit/9df9c2742861

src/js/background.js
@@ -35,9 +35,6 @@ if ( vAPI.webextFlavor === undefined ) {
 
 const µBlock = (function() { // jshint ignore:line
 
-    const oneSecond = 1000,
-          oneMinute = 60 * oneSecond;
-
     const hiddenSettingsDefault = {
         assetFetchTimeout: 30,
         autoCommentFilterTemplate: '{{date}} {{origin}}',
@@ -97,21 +94,19 @@ const µBlock = (function() { // jshint ignore:line
 
         hiddenSettingsDefault: hiddenSettingsDefault,
         hiddenSettings: (function() {
-            const out = Object.assign({}, hiddenSettingsDefault),
-                json = vAPI.localStorage.getItem('immediateHiddenSettings');
-            if ( typeof json === 'string' ) {
-                try {
-                    const o = JSON.parse(json);
-                    if ( o instanceof Object ) {
-                        for ( const k in o ) {
-                            if ( out.hasOwnProperty(k) ) {
-                                out[k] = o[k];
-                            }
-                        }
+            const out = Object.assign({}, hiddenSettingsDefault);
+            const json = vAPI.localStorage.getItem('immediateHiddenSettings');
+            if ( typeof json !== 'string' ) { return out; }
+            try {
+                const o = JSON.parse(json);
+                if ( o instanceof Object ) {
+                    for ( const k in o ) {
+                        if ( out.hasOwnProperty(k) ) { out[k] = o[k]; }
                     }
+                    self.log.verbosity = out.consoleLogLevel;
                 }
-                catch(ex) {
-                }
+            }
+            catch(ex) {
             }
             return out;
         })(),

src/js/console.js
@@ -29,6 +29,6 @@ self.log = (function() {
         set verbosity(level) {
             this.info = console.info = level === 'info' ? info : noopFunc;
         },
-        info,
+        info: noopFunc,
     };
 })();

src/js/storage.js
@@ -196,10 +196,9 @@
     vAPI.localStorage.setItem(
         'immediateHiddenSettings',
         JSON.stringify({
-                  cacheStorageAPI: this.hiddenSettings.cacheStorageAPI,
+                  consoleLogLevel: this.hiddenSettings.consoleLogLevel,
                disableWebAssembly: this.hiddenSettings.disableWebAssembly,
             suspendTabsUntilReady: this.hiddenSettings.suspendTabsUntilReady,
-            userResourcesLocation: this.hiddenSettings.userResourcesLocation,
         })
     );
 };