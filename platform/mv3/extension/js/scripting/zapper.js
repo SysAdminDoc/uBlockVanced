SysAdminDoc/uBlockVanced

commit 4d23e34dfcc551faeb67a91342ead4f4728d4af3

[mv3] Merge Safari branch
Safari version of uBO Lite can now be built from master branch.

Related issue:
https://github.com/uBlockOrigin/uBOL-home/issues/327

platform/mv3/extension/js/scripting/zapper.js
@@ -28,11 +28,13 @@ const zapper = self.uBOLZapper = self.uBOLZapper || {};
 if ( zapper.injected ) { return; }
 zapper.injected = true;
 
+const webext = typeof browser === 'object' ? browser : chrome;
+
 /******************************************************************************/
 
 const sendMessage = msg => {
     try {
-        chrome.runtime.sendMessage(msg).catch(( ) => { });
+        webext.runtime.sendMessage(msg).catch(( ) => { });
     } catch {
     }
 };
@@ -365,11 +367,10 @@ const onFrameMessage = function(msg) {
 // can remove the iframe.
 
 const bootstrap = async ( ) => {
-    const dynamicURL = new URL(chrome.runtime.getURL('/zapper-ui.html'));
+    const dynamicURL = new URL(webext.runtime.getURL('/zapper-ui.html'));
     return new Promise(resolve => {
         const frame = document.createElement('iframe');
         frame.setAttribute(zapperSecret, '');
-        document.documentElement.append(frame);
         frame.onload = ( ) => {
             frame.onload = null;
             frame.setAttribute(`${zapperSecret}-loaded`, '');
@@ -382,7 +383,7 @@ const bootstrap = async ( ) => {
                 quitZapper();
             };
             const realURL = new URL(dynamicURL);
-            realURL.hostname = chrome.i18n.getMessage('@@extension_id');
+            realURL.hostname = webext.i18n.getMessage('@@extension_id');
             frame.contentWindow.postMessage(
                 { what: 'zapperStart' },
                 realURL.origin,
@@ -394,7 +395,13 @@ const bootstrap = async ( ) => {
                 zapperFramePort: port,
             });
         };
-        frame.contentWindow.location = dynamicURL.href;
+        if ( dynamicURL.protocol !== 'safari-web-extension:' ) {
+            document.documentElement.append(frame);
+            frame.contentWindow.location = dynamicURL.href;
+        } else {
+            frame.setAttribute('src', dynamicURL.href);
+            document.documentElement.append(frame);
+        }
     });
 };
 

Makefile
@@ -2,7 +2,7 @@
 run_options := $(filter-out $@,$(MAKECMDGOALS))
 
 .PHONY: all clean cleanassets test lint chromium opera firefox npm dig \
-	mv3-chromium mv3-firefox mv3-edge \
+	mv3-chromium mv3-firefox mv3-edge mv3-safari \
 	compare maxcost medcost mincost modifiers record wasm
 
 sources := $(wildcard assets/* assets/*/* dist/version src/* src/*/* src/*/*/* src/*/*/*/*)
@@ -79,6 +79,11 @@ dist/build/uBOLite.edge: tools/make-mv3.sh tools/make-edge.mjs $(sources) $(plat
 	tools/make-mv3.sh edge
 
 mv3-edge: dist/build/uBOLite.edge
+
+dist/build/uBOLite.safari: tools/make-mv3.sh $(sources) $(platform) $(mv3-data) dist/build/mv3-data
+	tools/make-mv3.sh safari
+
+mv3-safari: dist/build/uBOLite.safari
 
 dist/build/uAssets:
 	tools/pull-assets.sh

platform/mv3/extension/js/admin.js
@@ -37,7 +37,7 @@ import {
 } from './mode-manager.js';
 
 import { broadcastMessage } from './utils.js';
-import { dnr } from './ext.js';
+import { dnr } from './ext-compat.js';
 import { registerInjectables } from './scripting-manager.js';
 import { rulesetConfig } from './config.js';
 import { ubolLog } from './debug.js';

platform/mv3/extension/js/background.js
@@ -44,7 +44,6 @@ import {
 
 import {
     browser,
-    dnr,
     localRead, localRemove, localWrite,
     runtime,
     windows,
@@ -75,11 +74,12 @@ import {
     saveRulesetConfig,
 } from './config.js';
 
+import { dnr } from './ext-compat.js';
 import { registerInjectables } from './scripting-manager.js';
 
 /******************************************************************************/
 
-const UBOL_ORIGIN = runtime.getURL('').replace(/\/$/, '');
+const UBOL_ORIGIN = runtime.getURL('').replace(/\/$/, '').toLowerCase();
 
 const canShowBlockedCount = typeof dnr.setExtensionActionOptions === 'function';
 
@@ -205,7 +205,9 @@ function onMessage(request, sender, callback) {
 
     // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/MessageSender
     //   Firefox API does not set `sender.origin`
-    if ( sender.origin !== undefined && sender.origin !== UBOL_ORIGIN ) { return; }
+    if ( sender.origin !== undefined ) {
+        if ( sender.origin.toLowerCase() !== UBOL_ORIGIN ) { return; }
+    }
 
     switch ( request.what ) {
 

platform/mv3/extension/js/debug.js
@@ -19,11 +19,19 @@
     Home: https://github.com/gorhill/uBlock
 */
 
-import { dnr } from './ext.js';
+import { INITIATOR_DOMAINS, dnr } from './ext-compat.js';
+import { browser } from './ext.js';
 
 /******************************************************************************/
 
-export const isSideloaded = dnr.onRuleMatchedDebug instanceof Object;
+const isModern = dnr.onRuleMatchedDebug instanceof Object;
+
+export const isSideloaded = (( ) => {
+    if ( isModern ) { return true; }
+    if ( typeof dnr.getMatchedRules === 'function' ) { return true; }
+    const manifest = browser.runtime.getManifest();
+    return manifest.permissions?.includes('declarativeNetRequestFeedback') ?? false;
+})();
 
 /******************************************************************************/
 
@@ -67,8 +75,8 @@ const getRuleset = async rulesetId => {
             if ( condition.requestDomains ) {
                 condition.requestDomains = pruneLongLists(condition.requestDomains);
             }
-            if ( condition.initiatorDomains ) {
-                condition.initiatorDomains = pruneLongLists(condition.initiatorDomains);
+            if ( condition[INITIATOR_DOMAINS] ) {
+                condition[INITIATOR_DOMAINS] = pruneLongLists(condition[INITIATOR_DOMAINS]);
             }
         }
         const ruleId = rule.id;
@@ -92,20 +100,32 @@ export const getMatchedRules = (( ) => {
     const noopFn = ( ) => Promise.resolve([]);
     if ( isSideloaded !== true ) { return noopFn; }
 
-    return async tabId => {
-        const promises = [];
-        for ( let i = 0; i < bufferSize; i++ ) {
-            const j = (writePtr + i) % bufferSize;
-            const ruleInfo = matchedRules[j];
-            if ( ruleInfo === null ) { continue; }
-            if ( ruleInfo.request.tabId !== -1 ) {
-                if ( ruleInfo.request.tabId !== tabId ) { continue; }
+    if ( isModern ) {
+        return async tabId => {
+            const promises = [];
+            for ( let i = 0; i < bufferSize; i++ ) {
+                const j = (writePtr + i) % bufferSize;
+                const ruleInfo = matchedRules[j];
+                if ( ruleInfo === null ) { continue; }
+                if ( ruleInfo.request.tabId !== -1 ) {
+                    if ( ruleInfo.request.tabId !== tabId ) { continue; }
+                }
+                const promise = getRuleDetails(ruleInfo);
+                if ( promise === undefined ) { continue; }
+                promises.unshift(promise);
             }
-            const promise = getRuleDetails(ruleInfo);
-            if ( promise === undefined ) { continue; }
-            promises.unshift(promise);
+            return Promise.all(promises);
+        };
+    }
+
+    return async tabId => {
+        const matchedRules = await dnr.getMatchedRules({ tabId });
+        if ( matchedRules instanceof Object === false ) { return []; }
+        const out = [];
+        for ( const ruleInfo of matchedRules.rulesMatchedInfo ) {
+            out.push({ request: ruleInfo.request });
         }
-        return Promise.all(promises);
+        return out;
     };
 })();
 
@@ -118,6 +138,7 @@ const matchedRuleListener = ruleInfo => {
 
 export const toggleDeveloperMode = state => {
     if ( isSideloaded !== true ) { return; }
+    if ( isModern === false ) { return; } 
     if ( state ) {
         dnr.onRuleMatchedDebug.addListener(matchedRuleListener);
     } else {

platform/mv3/extension/js/ext-compat.js
@@ -1,0 +1,96 @@
+/*******************************************************************************
+
+    uBlock Origin Lite - a comprehensive, MV3-compliant content blocker
+    Copyright (C) 2022-present Raymond Hill
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see {http://www.gnu.org/licenses/}.
+
+    Home: https://github.com/gorhill/uBlock
+*/
+
+export const webext = self.browser || self.chrome;
+export const dnr = webext.declarativeNetRequest;
+export const INITIATOR_DOMAINS = 'initiatorDomains';
+export const EXCLUDED_INITIATOR_DOMAINS = 'excludedInitiatorDomains';
+
+/******************************************************************************/
+
+const ruleCompare = (a, b) => a.id - b.id;
+
+const isSameRules = (a, b) => {
+    a.sort(ruleCompare);
+    b.sort(ruleCompare);
+    return JSON.stringify(a) === JSON.stringify(b);
+};
+
+/******************************************************************************/
+
+dnr.setAllowAllRules = async function(id, allowed, notAllowed, reverse) {
+    const [
+        beforeDynamicRules,
+        beforeSessionRules,
+    ] = await Promise.all([
+        dnr.getDynamicRules({ ruleIds: [ id+0 ] }),
+        dnr.getSessionRules({ ruleIds: [ id+1 ] }),
+    ]);
+    const addDynamicRules = [];
+    const addSessionRules = [];
+    if ( reverse || allowed.length || notAllowed.length ) {
+        const rule0 = {
+            id: id+0,
+            action: { type: 'allowAllRequests' },
+            condition: {
+                resourceTypes: [ 'main_frame' ],
+            },
+            priority: 1000000,
+        };
+        if ( allowed.length ) {
+            rule0.condition.requestDomains = allowed.slice();
+        } else if ( notAllowed.length ) {
+            rule0.condition.excludedRequestDomains = notAllowed.slice();
+        }
+        addDynamicRules.push(rule0);
+        // https://github.com/uBlockOrigin/uBOL-home/issues/114
+        // https://github.com/uBlockOrigin/uBOL-home/issues/247
+        const rule1 = {
+            id: id+1,
+            action: { type: 'allow' },
+            condition: {
+                tabIds: [ webext.tabs.TAB_ID_NONE ],
+            },
+            priority: 1000000,
+        };
+        if ( allowed.length ) {
+            rule1.condition.initiatorDomains = allowed.slice();
+        } else if ( notAllowed.length ) {
+            rule1.condition.excludedInitiatorDomains = notAllowed.slice();
+        }
+        addSessionRules.push(rule1);
+    }
+    if ( isSameRules(addDynamicRules, beforeDynamicRules) ) { return false; }
+    return Promise.all([
+        dnr.updateDynamicRules({
+            addRules: addDynamicRules,
+            removeRuleIds: beforeDynamicRules.map(r => r.id),
+        }),
+        dnr.updateSessionRules({
+            addRules: addSessionRules,
+            removeRuleIds: beforeSessionRules.map(r => r.id),
+        }),
+    ]).then(( ) =>
+        true
+    ).catch(( ) =>
+        false
+    );
+};

platform/mv3/extension/js/ext.js
@@ -19,16 +19,13 @@
     Home: https://github.com/gorhill/uBlock
 */
 
-export const browser =
-    self.browser instanceof Object &&
-    self.browser instanceof Element === false
-        ? self.browser
-        : self.chrome;
+import { webext } from './ext-compat.js';
 
-export const dnr = browser.declarativeNetRequest;
+/******************************************************************************/
+
+export const browser = webext;
 export const i18n = browser.i18n;
 export const runtime = browser.runtime;
-export const TAB_ID_NONE = browser.tabs.TAB_ID_NONE;
 export const windows = browser.windows;
 
 /******************************************************************************/

platform/mv3/extension/js/popup.js
@@ -347,6 +347,7 @@ async function init() {
         }
         tabURL.href = url.href || '';
     } catch {
+        return false;
     }
 
     if ( url !== undefined ) {

commit 244df3d70848e2607f1d1509030ead1577bac6b1

[mv3] Fix zapper
Regression from:
https://github.com/gorhill/uBlock/commit/4d23e34dfcc551faeb67a91342ead4f4728d4af3

platform/mv3/extension/js/scripting/zapper.js
@@ -371,7 +371,7 @@ const bootstrap = async ( ) => {
     return new Promise(resolve => {
         const frame = document.createElement('iframe');
         frame.setAttribute(zapperSecret, '');
-        frame.onload = ( ) => {
+        const onZapperLoad = ( ) => {
             frame.onload = null;
             frame.setAttribute(`${zapperSecret}-loaded`, '');
             const channel = new MessageChannel();
@@ -395,13 +395,11 @@ const bootstrap = async ( ) => {
                 zapperFramePort: port,
             });
         };
-        if ( dynamicURL.protocol !== 'safari-web-extension:' ) {
-            document.documentElement.append(frame);
+        frame.onload = ( ) => {
+            frame.onload = onZapperLoad;
             frame.contentWindow.location = dynamicURL.href;
-        } else {
-            frame.setAttribute('src', dynamicURL.href);
-            document.documentElement.append(frame);
-        }
+        };
+        document.documentElement.append(frame);
     });
 };
 