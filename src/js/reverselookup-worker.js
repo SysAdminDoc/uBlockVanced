SysAdminDoc/uBlockVanced

commit 83b9b0487e8ff615e3e2a0c2c5dede47a82c1b3f

Add path support as target option in static extended filtering
Support for paths allows to narrow down specific static extended
filters to specific webpages on a given site.

Examples of usage:

example.com/toto##h1
/example\.com\/toto\d+/#@#h1

src/js/reverselookup-worker.js
@@ -115,7 +115,8 @@ const fromExtendedFilter = function(details) {
     const exception = prefix.charAt(1) === '@';
     const selector = details.rawFilter.slice(prefix.length);
     const isHtmlFilter = prefix.endsWith('^');
-    const hostname = details.hostname;
+    const url = new URL(details.url || 'about:blank');
+    const { hostname, pathname } = url;
 
     // The longer the needle, the lower the number of false positives.
     // https://github.com/uBlockOrigin/uBlock-issues/issues/1139
@@ -147,15 +148,41 @@ const fromExtendedFilter = function(details) {
         }
     }
 
-    const hostnameMatches = hn => {
-        if ( hn === '' ) { return true; }
-        if ( hn.charCodeAt(0) === 0x2F /* / */ ) {
-            return (new RegExp(hn.slice(1,-1))).test(hostname);
-        }
-        if ( reHostname.test(hn) ) { return true; }
+    const hostnameTargetMatchesHostname = target => {
+        if ( target === '' ) { return true; }
+        if ( reHostname.test(target) ) { return true; }
         if ( reEntity === undefined ) { return false; }
-        if ( reEntity.test(hn) ) { return true; }
+        if ( reEntity.test(target) ) { return true; }
         return false;
+    };
+
+    const regexTargetMatchesHostname = target => {
+        return (new RegExp(target)).test(hostname);
+    };
+
+    const regexTargetMatchesURL = target => {
+        const pathPos = target.indexOf('\\/');
+        if ( pathPos === -1 ) {
+            return regexTargetMatchesHostname(target.slice(1, -1));
+        }
+        return regexTargetMatchesHostname(`${target.slice(1, pathPos)}$`) &&
+            (new RegExp(`^${target.slice(pathPos, -1)}`)).test(pathname);
+    };
+
+    const pathTargetMatchesURL = target => {
+        const pathPos = target.indexOf('/');
+        return hostnameTargetMatchesHostname(target.slice(0, pathPos)) &&
+            pathname.startsWith(target.slice(pathPos));
+    };
+
+    const targetMatchesURL = target => {
+        if ( target.charCodeAt(0) === 0x2F /* / */ ) {
+            return regexTargetMatchesURL(target.slice(1, -1));
+        }
+        if ( target.includes('/') ) {
+            return pathTargetMatchesURL(target);
+        }
+        return hostnameTargetMatchesHostname(target);
     };
 
     const response = Object.create(null);
@@ -212,24 +239,25 @@ const fromExtendedFilter = function(details) {
             // Response header filtering
             /* fallthrough */
             case 64: {
-                if ( exception !== ((fargs[2] & 0b001) !== 0) ) { break; }
+                if ( exception !== (fargs[2].charCodeAt(0) === 0x2D /* - */) ) { break; }
+                const candidate = fargs[2].slice(1);
                 if ( /^responseheader\(.+\)$/.test(selector) ) {
-                    if ( fargs[3] !== needle ) { break; }
-                    if ( hostnameMatches(fargs[1]) === false ) { break; }
+                    if ( candidate !== needle ) { break; }
+                    if ( targetMatchesURL(fargs[1]) === false ) { break; }
                     found = fargs[1] + prefix + selector;
                     break;
                 }
-                const isProcedural = (fargs[2] & 0b010) !== 0;
+                const isProcedural = candidate.charCodeAt(0) === 0x7B /* { */;
                 if (
-                    isProcedural === false && fargs[3] !== selector ||
-                    isProcedural && JSON.parse(fargs[3]).raw !== selector
+                    isProcedural === false && candidate !== selector ||
+                    isProcedural && JSON.parse(candidate).raw !== selector
                 ) {
                     break;
                 }
-                if ( hostnameMatches(fargs[1]) === false ) { break; }
+                if ( targetMatchesURL(fargs[1]) === false ) { break; }
                 // https://www.reddit.com/r/uBlockOrigin/comments/d6vxzj/
                 //   Ignore match if specific cosmetic filters are disabled
-                if (
+                if ( 
                     filterType === 8 &&
                     exception === false &&
                     details.ignoreSpecific
@@ -240,13 +268,15 @@ const fromExtendedFilter = function(details) {
                 break;
             }
             // Scriptlet injection
-            case 32:
-                if ( exception !== ((fargs[2] & 0b001) !== 0) ) { break; }
-                if ( fargs[3] !== details.needle ) { break; }
-                if ( hostnameMatches(fargs[1]) ) {
+            case 32: {
+                if ( exception !== (fargs[2].charCodeAt(0) === 0x2D /* - */) ) { break; }
+                const candidate = fargs[2].slice(1);
+                if ( candidate !== details.needle ) { break; }
+                if (targetMatchesURL(fargs[1]) ) {
                     found = fargs[1] + prefix + selector;
                 }
                 break;
+            }
             default:
                 break;
             }

platform/chromium/vapi-background-ext.js
@@ -212,7 +212,7 @@ vAPI.scriptletsInjector = (( ) => {
     const parts = [
         '(',
         function(details) {
-            if ( typeof self.uBO_scriptletsInjected === 'string' ) { return; }
+            if ( self.uBO_scriptletsInjected !== undefined ) { return; }
             const doc = document;
             const { location } = doc;
             if ( location === null ) { return; }

platform/firefox/vapi-background-ext.js
@@ -355,7 +355,7 @@ vAPI.scriptletsInjector = (( ) => {
     const parts = [
         '(',
         function(details) {
-            if ( typeof self.uBO_scriptletsInjected === 'string' ) { return; }
+            if ( self.uBO_scriptletsInjected !== undefined ) { return; }
             const doc = document;
             const { location } = doc;
             if ( location === null ) { return; }

src/js/background.js
@@ -180,8 +180,8 @@ const µBlock = {  // jshint ignore:line
 
     // Read-only
     systemSettings: {
-        compiledMagic: 57,  // Increase when compiled format changes
-        selfieMagic: 59,    // Increase when selfie format changes
+        compiledMagic: 60,  // Increase when compiled format changes
+        selfieMagic: 60,    // Increase when selfie format changes
     },
 
     // https://github.com/uBlockOrigin/uBlock-issues/issues/759#issuecomment-546654501

src/js/codemirror/ubo-static-filtering.js
@@ -25,6 +25,7 @@
 
 import * as sfp from '../static-filtering-parser.js';
 import { dom, qs$ } from '../dom.js';
+import { tokenizableStrFromRegex } from '../regex-analyzer.js';
 
 /******************************************************************************/
 
@@ -66,6 +67,8 @@ const uBOStaticFilteringMode = (( ) => {
             node || mode.currentWalkerNode, sfp.NODE_FLAG_ERROR
         ) !== 0;
     };
+
+    const reGoodRegexToken = /[^\x01%0-9A-Za-z][%0-9A-Za-z]{7,}|[^\x01%0-9A-Za-z][%0-9A-Za-z]{1,6}[^\x01%0-9A-Za-z]/;
 
     const colorFromAstNode = mode => {
         if ( mode.astParser.nodeIsEmptyString(mode.currentWalkerNode) ) { return '+'; }
@@ -119,7 +122,9 @@ const uBOStaticFilteringMode = (( ) => {
         case sfp.NODE_TYPE_NET_PATTERN:
             if ( mode.astWalker.canGoDown() ) { break; }
             if ( mode.astParser.isRegexPattern() ) {
-                if ( mode.astParser.getNodeFlags(mode.currentWalkerNode, sfp.NODE_FLAG_PATTERN_UNTOKENIZABLE) !== 0 ) {
+                const s = mode.astParser.getNodeString(mode.currentWalkerNode);
+                const tokenizable = tokenizableStrFromRegex(s);
+                if ( reGoodRegexToken.test(tokenizable) === false ) {
                     return 'variable warning';
                 }
                 return 'variable notice';

src/js/contentscript.js
@@ -1312,7 +1312,7 @@ vAPI.DOMFilterer = class {
         vAPI.messaging.send('contentscript', {
             what: 'retrieveContentScriptParameters',
             url: vAPI.effectiveSelf.location.href,
-            needScriptlets: typeof self.uBO_scriptletsInjected !== 'string',
+            needScriptlets: self.uBO_scriptletsInjected === undefined,
         }).then(response => {
             onResponseReady(response);
         });

src/js/cosmetic-filtering.js
@@ -235,7 +235,7 @@ const CosmeticFilteringEngine = function() {
     });
 
     // specific filters
-    this.specificFilters = new StaticExtFilteringHostnameDB(2);
+    this.specificFilters = new StaticExtFilteringHostnameDB();
 
     // low generic cosmetic filters: map of hash => stringified selector list
     this.lowlyGeneric = new Map();
@@ -254,16 +254,6 @@ const CosmeticFilteringEngine = function() {
         str: '',
         mru: new MRUCache(16)
     };
-
-    // Short-lived: content is valid only during one function call. These
-    // is to prevent repeated allocation/deallocation overheads -- the
-    // constructors/destructors of javascript Set/Map is assumed to be costlier
-    // than just calling clear() on these.
-    this.$specificSet = new Set();
-    this.$exceptionSet = new Set();
-    this.$proceduralSet = new Set();
-    this.$dummySet = new Set();
-
     this.reset();
 };
 
@@ -429,7 +419,7 @@ CosmeticFilteringEngine.prototype.compileGenericUnhideSelector = function(
     //   hostnames). No distinction is made between declarative and
     //   procedural selectors, since they really exist only to cancel
     //   out other cosmetic filters.
-    writer.push([ 8, '', 0b001, compiled ]);
+    writer.push([ 8, '', `-${compiled}` ]);
 };
 
 /******************************************************************************/
@@ -452,23 +442,8 @@ CosmeticFilteringEngine.prototype.compileSpecificSelector = function(
     }
 
     writer.select('COSMETIC_FILTERS:SPECIFIC');
-
-    // https://github.com/chrisaljoudi/uBlock/issues/145
-    let unhide = exception ? 1 : 0;
-    if ( not ) { unhide ^= 1; }
-
-    let kind = 0;
-    if ( unhide === 1 ) {
-        kind |= 0b001;     // Exception
-    }
-    if ( compiled.charCodeAt(0) === 0x7B /* '{' */ ) {
-        kind |= 0b010;     // Procedural
-    }
-    if ( hostname === '*' ) {
-        kind |= 0b100;     // Applies everywhere
-    }
-
-    writer.push([ 8, hostname, kind, compiled ]);
+    const prefix = ((exception ? 1 : 0) ^ (not ? 1 : 0)) ? '-' : '+';
+    writer.push([ 8, hostname, `${prefix}${compiled}` ]);
 };
 
 /******************************************************************************/
@@ -500,15 +475,16 @@ CosmeticFilteringEngine.prototype.fromCompiledContent = function(reader, options
         //   not to be injected conditionally through the DOM surveyor.
         //   hash,  *, .promoted-tweet
         case 8:
-            if ( args[2] === 0b100 ) {
-                if ( this.reSimpleHighGeneric.test(args[3]) )
-                    this.highlyGeneric.simple.dict.add(args[3]);
+            if ( args[1] === '*' ) {
+                const selector = args[2].slice(1);
+                if ( this.reSimpleHighGeneric.test(selector) )
+                    this.highlyGeneric.simple.dict.add(selector);
                 else {
-                    this.highlyGeneric.complex.dict.add(args[3]);
+                    this.highlyGeneric.complex.dict.add(selector);
                 }
                 break;
             }
-            this.specificFilters.store(args[1], args[2] & 0b011, args[3]);
+            this.specificFilters.store(args[1], args[2]);
             break;
         default:
             this.discardedCount += 1;
@@ -590,9 +566,7 @@ CosmeticFilteringEngine.prototype.toSelfie = function() {
 
 CosmeticFilteringEngine.prototype.fromSelfie = function(selfie) {
     if ( selfie.version !== this.selfieVersion ) {
-        throw new Error(
-            `cosmeticFilteringEngine: mismatched selfie version, ${selfie.version}, expected ${this.selfieVersion}`
-        );
+        throw new TypeError('Bad selfie');
     }
     this.acceptedCount = selfie.acceptedCount;
     this.discardedCount = selfie.discardedCount;
@@ -630,16 +604,11 @@ CosmeticFilteringEngine.prototype.removeFromSelectorCache = function(
     type = undefined
 ) {
     const targetHostnameLength = targetHostname.length;
-    for ( let entry of this.selectorCache ) {
-        let hostname = entry[0];
-        let item = entry[1];
+    for ( const [ hostname, item ] of this.selectorCache ) {
         if ( targetHostname !== '*' ) {
             if ( hostname.endsWith(targetHostname) === false ) { continue; }
-            if (
-                hostname.length !== targetHostnameLength &&
-                hostname.charAt(hostname.length - targetHostnameLength - 1) !== '.'
-            ) {
-                continue;
+            if ( hostname.length !== targetHostnameLength ) {
+                if ( hostname.at(-1) !== '.' ) { continue; }
             }
         }
         item.remove(type);
@@ -791,12 +760,8 @@ CosmeticFilteringEngine.prototype.retrieveSpecificSelectors = function(
         options.noSpecificCosmeticFiltering !== true ||
         options.noGenericCosmeticFiltering !== true
     ) {
-        const specificSet = this.$specificSet;
-        const proceduralSet = this.$proceduralSet;
-        const exceptionSet = this.$exceptionSet;
-        const dummySet = this.$dummySet;
-
         // Cached cosmetic filters: these are always declarative.
+        const specificSet = new Set();
         if ( cacheEntry !== undefined ) {
             cacheEntry.retrieveCosmetic(specificSet, out.genericCosmeticHashes = []);
             if ( cacheEntry.disableSurveyor ) {
@@ -804,35 +769,30 @@ CosmeticFilteringEngine.prototype.retrieveSpecificSelectors = function(
             }
         }
 
+        const allSet = new Set();
         // Retrieve filters with a non-empty hostname
-        const retrieveSets = [ specificSet, exceptionSet, proceduralSet, exceptionSet ];
-        const discardSets = [ dummySet, exceptionSet ];
-        this.specificFilters.retrieve(
-            hostname,
-            options.noSpecificCosmeticFiltering ? discardSets : retrieveSets,
-            1
-        );
-        // Retrieve filters with a regex-based hostname value
-        this.specificFilters.retrieve(
-            hostname,
-            options.noSpecificCosmeticFiltering ? discardSets : retrieveSets,
-            3
-        );
+        this.specificFilters.retrieveSpecifics(allSet, hostname);
         // Retrieve filters with a entity-based hostname value
         const entity = entityFromHostname(hostname, request.domain);
-        if ( entity !== '' ) {
-            this.specificFilters.retrieve(
-                entity,
-                options.noSpecificCosmeticFiltering ? discardSets : retrieveSets,
-                1
-            );
-        }
+        this.specificFilters.retrieveSpecifics(allSet, entity);
+        // Retrieve filters with a regex-based hostname value
+        this.specificFilters.retrieveSpecificsByRegex(allSet, hostname, request.url);
         // Retrieve filters with an empty hostname
-        this.specificFilters.retrieve(
-            hostname,
-            options.noGenericCosmeticFiltering ? discardSets : retrieveSets,
-            2
-        );
+        this.specificFilters.retrieveGenerics(allSet);
+
+        // Split filters in different groups
+        const proceduralSet = new Set();
+        const exceptionSet = new Set();
+        for ( const s of allSet ) {
+            const selector = s.slice(1);
+            if ( s.charCodeAt(0) === 0x2D /* - */ ) {
+                exceptionSet.add(selector);
+            } else if ( selector.charCodeAt(0) === 0x7B /* { */ ) {
+                proceduralSet.add(selector);
+            } else {
+                specificSet.add(selector);
+            }
+        }
 
         // Apply exceptions to specific filterset
         if ( exceptionSet.size !== 0 ) {
@@ -855,12 +815,12 @@ CosmeticFilteringEngine.prototype.retrieveSpecificSelectors = function(
         // filters, so we extract and inject them immediately.
         if ( proceduralSet.size !== 0 ) {
             for ( const json of proceduralSet ) {
-                const pfilter = JSON.parse(json);
                 if ( exceptionSet.has(json) ) {
                     proceduralSet.delete(json);
                     out.exceptedFilters.push(json);
                     continue;
                 }
+                const pfilter = JSON.parse(json);
                 if ( exceptionSet.has(pfilter.raw) ) {
                     proceduralSet.delete(json);
                     out.exceptedFilters.push(pfilter.raw);
@@ -914,12 +874,6 @@ CosmeticFilteringEngine.prototype.retrieveSpecificSelectors = function(
                 }
             }
         }
-
-        // Important: always clear used registers before leaving.
-        specificSet.clear();
-        proceduralSet.clear();
-        exceptionSet.clear();
-        dummySet.clear();
     }
 
     const details = {

src/js/html-filtering.js
@@ -20,7 +20,7 @@
 */
 
 import { StaticExtFilteringHostnameDB } from './static-ext-filtering-db.js';
-import { entityFromDomain } from './uri-utils.js';
+import { entityFromHostname } from './uri-utils.js';
 import logger from './logger.js';
 import { sessionFirewall } from './filtering-engines.js';
 import µb from './background.js';
@@ -30,7 +30,7 @@ import µb from './background.js';
 const pselectors = new Map();
 const duplicates = new Set();
 
-const filterDB = new StaticExtFilteringHostnameDB(2);
+const filterDB = new StaticExtFilteringHostnameDB();
 
 let acceptedCount = 0;
 let discardedCount = 0;
@@ -260,7 +260,7 @@ function logOne(details, exception, selector) {
         .setDocOriginFromURL(details.url)
         .setFilter({
             source: 'extended',
-            raw: `${exception === 0 ? '##' : '#@#'}^${selector}`
+            raw: `${exception === 0 ? '##' : '#@#'}^${selector}`,
         })
         .toLogger();
 }
@@ -338,16 +338,11 @@ htmlFilteringEngine.compile = function(parser, writer) {
     let hasOnlyNegated = true;
     for ( const { hn, not, bad } of parser.getExtFilterDomainIterator() ) {
         if ( bad ) { continue; }
-        let kind = isException ? 0b01 : 0b00;
-        if ( not ) {
-            kind ^= 0b01;
-        } else {
+        const prefix = ((isException ? 1 : 0) ^ (not ? 1 : 0)) ? '-' : '+';
+        if ( not === false ) {
             hasOnlyNegated = false;
         }
-        if ( compiled.charCodeAt(0) === 0x7B /* '{' */ ) {
-            kind |= 0b10;
-        }
-        compiledFilters.push([ 64, hn, kind, compiled ]);
+        compiledFilters.push([ 64, hn, `${prefix}${compiled}` ]);
     }
 
     // Not allowed since it's equivalent to forbidden generic HTML filters
@@ -373,59 +368,45 @@ htmlFilteringEngine.fromCompiledContent = function(reader) {
         }
         duplicates.add(fingerprint);
         const args = reader.args();
-        filterDB.store(args[1], args[2], args[3]);
+        filterDB.store(args[1], args[2]);
     }
 };
 
 htmlFilteringEngine.retrieve = function(fctxt) {
-    const plains = new Set();
-    const procedurals = new Set();
-    const exceptions = new Set();
-    const retrieveSets = [ plains, exceptions, procedurals, exceptions ];
-
+    const all = new Set();
     const hostname = fctxt.getHostname();
-    filterDB.retrieve(hostname, retrieveSets);
-
-    const domain = fctxt.getDomain();
-    const entity = entityFromDomain(domain);
-    const hostnameEntity = entity !== ''
-        ? `${hostname.slice(0, -domain.length)}${entity}`
-        : '*';
-    filterDB.retrieve(hostnameEntity, retrieveSets, 1);
-
-    if ( plains.size === 0 && procedurals.size === 0 ) { return; }
+    filterDB.retrieveSpecifics(all, hostname);
+    const entity = entityFromHostname(hostname, fctxt.getDomain());
+    filterDB.retrieveSpecifics(all, entity);
+    filterDB.retrieveSpecificsByRegex(all, hostname, fctxt.url);
+    filterDB.retrieveGenerics(all);
+    if ( all.size === 0 ) { return; }
 
     // https://github.com/gorhill/uBlock/issues/2835
     //   Do not filter if the site is under an `allow` rule.
-    if (
-        µb.userSettings.advancedUserEnabled &&
-        sessionFirewall.evaluateCellZY(hostname, hostname, '*') === 2
-    ) {
-        return;
+    if ( µb.userSettings.advancedUserEnabled ) {
+        if ( sessionFirewall.evaluateCellZY(hostname, hostname, '*') === 2 ) { return; }
     }
 
-    const out = { plains, procedurals };
-
-    if ( exceptions.size === 0 ) {
-        return out;
-    }
-
-    for ( const selector of exceptions ) {
-        if ( plains.has(selector) ) {
-            plains.delete(selector);
-            logOne(fctxt, 1, selector);
-            continue;
-        }
-        if ( procedurals.has(selector) ) {
-            procedurals.delete(selector);
-            logOne(fctxt, 1, JSON.parse(selector).raw);
-            continue;
+    // Split filters in different groups
+    const plains = new Set();
+    const procedurals = new Set();
+    for ( const s of all ) {
+        if ( s.charCodeAt(0) === 0x2D /* - */ ) { continue; }
+        const selector = s.slice(1);
+        const isProcedural = selector.startsWith('{');
+        if ( all.has(`-${selector}`) ) {
+            logOne(fctxt, 1, isProcedural ? JSON.parse(selector).raw : selector);
+        } else if ( isProcedural ) {
+            procedurals.add(selector);
+        } else {
+            plains.add(selector);
         }
     }
 
-    if ( plains.size !== 0 || procedurals.size !== 0 ) {
-        return out;
-    }
+    if ( plains.size === 0 && procedurals.size === 0 ) { return; }
+
+    return { plains, procedurals };
 };
 
 htmlFilteringEngine.apply = function(doc, details, selectors) {

commit ced573ab9bbec88bcd6a3c2decdc97b6a955c188

Fix broken reverse lookup of filter lists
Related feedback:
https://github.com/uBlockOrigin/uBlock-issues/discussions/3684

Regression from:
https://github.com/gorhill/uBlock/commit/83b9b0487e8ff615e3e2a0c2c5dede47a82c1b3f

src/js/reverselookup-worker.js
@@ -163,7 +163,7 @@ const fromExtendedFilter = function(details) {
     const regexTargetMatchesURL = target => {
         const pathPos = target.indexOf('\\/');
         if ( pathPos === -1 ) {
-            return regexTargetMatchesHostname(target.slice(1, -1));
+            return regexTargetMatchesHostname(target);
         }
         return regexTargetMatchesHostname(`${target.slice(1, pathPos)}$`) &&
             (new RegExp(`^${target.slice(pathPos, -1)}`)).test(pathname);